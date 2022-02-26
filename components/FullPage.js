dccf3c31924c649bbeb367c5e1859bf1d5bd7a3c
Cleisson Vieira <cleissonvb@gmail.com>
2022-02-26 17:38:27 -0300

    feat: add nav bar

--- components/FullPage.js
+++ components/FullPage.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import navStyles from '../styles/navbar.module.css'
 
-export default function FullPage({ children, pageProps }) {
+export default function FullPage({ children, pageProps, hasHeader=false }) {
   return (
+  <>
+    {hasHeader &&
+      <nav className={navStyles.nav}>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/portfolio">Portfolio</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/contacts">Contact</a></li>
+        </ul>
+      </nav>
+    }
     <div className={styles.container}>
       <Head>
         <title>{pageProps.meta.title}</title>
@@ -24,5 +36,6 @@ export default function FullPage({ children, pageProps }) {
         </a>
       </footer>
     </div>
+  </>
   )
 }
