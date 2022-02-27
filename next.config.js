5704e83e41b14c808392b50c98444b15f3aef3e7
Cleisson Vieira <cleissonvb@gmail.com>
2022-02-27 16:11:22 -0300

    fix: externalize config envs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
+const dotenv = require('dotenv');
+const { version, name } = require('./package.json');
+const NODE_ENV = process.env.ENV || process.env.NODE_ENV || 'development';
+
+// env
+const defaultEnv = './config/env/.env.default';
+const envPath = `./config/env/.env.${NODE_ENV}`;
+
+// call dotenv and it will return an Object with a parsed key
+const defaultEnvFile = dotenv.config({ path: defaultEnv }).parsed;
+const envPathFile = dotenv.config({ path: envPath }).parsed;
+
+// call dotenv and it will return an Object with a parsed key
+const env = {
+  ...defaultEnvFile,
+  ...envPathFile,
+  VERSION: version,
+  RELEASE: `${name}@${version}`,
+};
+// create a nice object from the env variable
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  let value = process.env[next] || env[next];
+  if (['true', 'false'].includes(env[next])) {
+    value = env[next].toLowerCase() === 'true';
+  }
+  prev[next] = value;
+  return prev;
+}, {});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: envKeys,
 }
 
 module.exports = nextConfig
