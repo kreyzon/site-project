a1ea9f02573753ae52db462010101e1c0c8aaad4
Cleisson Vieira <cleissonvb@gmail.com>
2022-02-28 12:31:33 -0300

    feat: add blog page as work in progress

---
+++ pages/blog.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import FullPage from '../components/layouts/FullPage'
+import styles from '../styles/Home.module.css'
+
+export default function Contact() {
+  const meta = {
+    title: "Blog",
+    description: "All my thoughts and studies are here!"
+  }
+  return (
+    <FullPage pageProps={{meta}} hasHeader>
+      <h1 className={styles.title}>
+        BLOG
+      </h1>
+      <div className={styles.description}>
+      <Image src="/wip.png" alt='Work In Progress' width="320px" height="280px"></Image>
+      </div>
+    </FullPage>
+  )
+}
