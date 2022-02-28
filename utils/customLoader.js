5f1a9cb71d3f1db0ceb54164161372482bbdeabc
Cleisson Vieira <cleissonvb@gmail.com>
2022-02-28 17:16:01 -0300

    fix: load images path in production

--- utils/customLoader.js
+++ utils/customLoader.js
@@ -1,6 +1,8 @@
 // opt-out of image optimization, no-op
 const customLoader = ({ src }) => {
-  return src
+  const NODE_ENV = process.env.ENV || process.env.NODE_ENV || 'development';
+  const isProd = NODE_ENV === 'production'
+  return isProd ? '/site-dashboard'+src : src
 }
 
 export default customLoader
